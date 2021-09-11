31a5e1e40dfa290a565a2cb236ce1659689a87f7
MUK-Dev <manian1221@gmail.com>
2021-09-11 20:57:17 +0500

    Added Answers service

---
+++ src/models/answers.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+// answers-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = "answers";
+  const mongooseClient = app.get("mongooseClient");
+  const { Schema } = mongooseClient;
+  const schema = new Schema(
+    {
+      user: {
+        user_id: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+        username: { type: String, required: true },
+        avatar: { type: String, required: true },
+      },
+      room: {
+        room_id: { type: mongoose.Types.ObjectId, required: true, ref: "Room" },
+        question: { type: String, required: true },
+      },
+      grade: { type: String, default: undefined },
+      text: { type: Object, required: true },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const users = require("./users/users.service.js");
 const rooms = require('./rooms/rooms.service.js');
 const messages = require('./messages/messages.service.js');
+const answers = require('./answers/answers.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(rooms);
   app.configure(messages);
+  app.configure(answers);
 };
